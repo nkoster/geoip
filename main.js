497a5bec6689dce2f0a0b194a707e9e5e6ba1d53
Niels Koster <n.koster@portavita.eu>
2020-03-08 16:48:03 +0100

    Emitter inside request

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const
     events = require('events'),
-    emmitter = new events.EventEmitter(),
     app = require('express')(),
     http = require('http').createServer(app),
     geoLoc = require('./util/geolocation')
 
 app.get('/geoip/:ip', (req, res) => {
+    const emmitter = new events.EventEmitter()
     geoLoc(req.params.ip, './IP2LOCATION-LITE-DB11.CSV', emmitter)
     emmitter.on('receive', data => {
+        console.log(req.params.ip, data)
         res.setHeader('Content-Type', 'application/json')
         res.end(data)
+        emmitter.removeAllListeners
     })
 })
 
-http.listen(9999, _ => {
-    console.log('http listening on :9999')
+http.listen(10000, _ => {
+    console.log('http listening on :10000')
 })
