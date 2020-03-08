f26bb3d8292dc996f60757d35206f936d6ed076e
Niels Koster <n.koster@portavita.eu>
2020-03-08 04:00:24 +0100

    Simplify

--- main.js
+++ main.js
@@ -5,6 +5,5 @@ const
     ip = process.argv[2] || '194.109.6.66'
 
 emmitter.on('receive', data => console.log(data))
-emmitter.emit('connect')
 
 geoLoc(ip, './IP2LOCATION-LITE-DB11.CSV', emmitter)
