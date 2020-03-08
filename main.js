b7a6e9f75a1cb97545027857fcac56d299c87d07
Niels Koster <n.koster@portavita.eu>
2020-03-08 03:52:29 +0100

    Simplify

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const
     events = require('events'),
     emmitter = new events.EventEmitter(),
-    dataReceive = data => {
-        console.log(data)
-    },
     geoLoc = require('./util/geolocation'),
     ip = process.argv[2] || '194.109.6.66'
 
-emmitter.on('receive', dataReceive)
+emmitter.on('receive', data => console.log(data))
 emmitter.emit('connect')
 
 geoLoc(ip, './IP2LOCATION-LITE-DB11.CSV', emmitter)
